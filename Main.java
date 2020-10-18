c940fe9919a1fe846a1bdcbf1aa1f140ded33b52
Will brown <wtbrown10@gmail.com>
2020-10-18 18:26:25 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.will;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte myAge = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println();
+    }
+}
